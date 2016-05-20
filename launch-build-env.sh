f1e530529586389c9882588589b7c4c7b7eebce7
Anthony ARNAUD <perso@anthony-arnaud.fr>
2016-05-21 00:28:13 +0200

    Add 5 sec sleep to wait DHCP in build env  script

--- launch-build-env.sh
+++ launch-build-env.sh
@@ -5,6 +5,7 @@ CONTAINER_NAME=lxd-webui
 lxc info lxd-webui &> /dev/null || {
     lxc launch images:ubuntu/xenial/amd64 ${CONTAINER_NAME} -c security.privileged=true
     lxc config device add ${CONTAINER_NAME} lxd-webui disk source=$PWD path=/home/ubuntu/lxd-webui
+    sleep 5 # Wait network DHCP
     lxc exec ${CONTAINER_NAME} -- apt-get update
     lxc exec ${CONTAINER_NAME} -- apt-get install -y apt-transport-https curl
     lxc exec ${CONTAINER_NAME} -- bash << EOF
