a50d8003d69bd0068f3d9974e33ca12adb5c94de
Anthony ARNAUD <perso@anthony-arnaud.fr>
2016-07-05 23:11:54 +0200

    Improve lxd build env script

--- launch-build-env.sh
+++ launch-build-env.sh
@@ -2,14 +2,14 @@
 
 CONTAINER_NAME=lxd-webui
 
-lxc info lxd-webui &> /dev/null || {
+lxc info ${CONTAINER_NAME} &> /dev/null || {
     lxc launch images:ubuntu/xenial/amd64 ${CONTAINER_NAME} -c security.privileged=true
     lxc config device add ${CONTAINER_NAME} lxd-webui disk source=$PWD path=/home/ubuntu/lxd-webui
     sleep 5 # Wait network DHCP
     lxc exec ${CONTAINER_NAME} -- apt-get update
-    lxc exec ${CONTAINER_NAME} -- apt-get install -y apt-transport-https curl
+    lxc exec ${CONTAINER_NAME} -- apt-get install -y apt-transport-https wget ca-certificates
     lxc exec ${CONTAINER_NAME} -- bash << EOF
-        curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+        wget -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
         echo 'deb https://deb.nodesource.com/node_4.x trusty main' > /etc/apt/sources.list.d/nodesource.list
         echo 'deb-src https://deb.nodesource.com/node_4.x trusty main' >> /etc/apt/sources.list.d/nodesource.list
 EOF
@@ -18,6 +18,6 @@ EOF
     lxc exec ${CONTAINER_NAME} -- gem install fpm
 }
 
-lxc list lxd-webui -c s | grep STOPPED &> /dev/null && lxc start lxd-webui
+lxc list ${CONTAINER_NAME} -c s | grep STOPPED &> /dev/null && lxc start ${CONTAINER_NAME}
 
 lxc exec ${CONTAINER_NAME} -- sudo -u ubuntu -i
